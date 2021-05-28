97f23a5afdc28d360281d7661c4748ae80867053
Ashutosh Nakhe <ashutoshnakhe@Ashutoshs-Macbook-Pro.local>
2021-05-28 15:02:16 +0530

    Enable Corse

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
-// const cors = require('cors')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT
@@ -18,7 +18,7 @@ const port = process.env.PORT
 //     res.status(503).send('Site is currently down. Check back soon!')
 // })
 
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 
